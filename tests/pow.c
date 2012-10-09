dfb45f19317ae1f3cd7c84b75641cc5a0ab6be8e
hasiek <hasiek01@gmail.com>
2012-10-09 22:26:36 +0200

    testy do Sita Eratostenesa.

---
+++ tests/pow.c
@@ -0,0 +1,11 @@
+// a bit of playing around with <math.h> and trying to work it out with ints
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	printf ("%f", pow(3,2));
+	
+	return 0;
+}
