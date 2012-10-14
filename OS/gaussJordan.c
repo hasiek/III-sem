21e7faea3c4cf4a97f42888129f426bcb1317c70
hasiek <hasiek01@gmail.com>
2012-10-14 17:46:23 +0200

    ukonczona implementacja algorytmu Gaussa-Jordana w C.

--- OS/gaussJordan.c
+++ OS/gaussJordan.c
@@ -1,53 +1,75 @@
 // Gauss-Jordan's Elimination implementation
-// 13.10.2012
+// 14.10.2012
 
 #include <stdio.h>
 
 int main() {
 	
-	float macierz[4][4];
-	float wektor[4];
-	float temp;
+	float macierz[3][4];
+	float wektor[3];
+	float temp[4];
 	int i,j,k;
 	int n = 4;
 	
 	macierz[0][0] = 1.0;
-	macierz[0][1] = 5.0;
-	macierz[0][2] = 9.0;
-	macierz[0][3] = 6.0;
-	macierz[1][0] = 7.0;
-	macierz[1][1] = 1.0;
-	macierz[1][2] = 3.0;
-	macierz[1][3] = 9.0;
-	macierz[2][0] = 12.0;
-	macierz[2][1] = 5.0;
-	macierz[2][2] = 7.0;
-	macierz[2][3] = 4.0;
-	macierz[3][0] = 10.0;
-	macierz[3][1] = 2.0;
-	macierz[3][2] = 8.0;
-	macierz[3][3] = 6.0;
+	macierz[0][1] = 2.0;
+	macierz[0][2] = 3.0;
+	macierz[0][3] = 14.0;
+	macierz[1][0] = 4.0;
+	macierz[1][1] = 3.0;
+	macierz[1][2] = -1.0;
+	macierz[1][3] = 7.0;
+	macierz[2][0] = 1.0;
+	macierz[2][1] = -1.0;
+	macierz[2][2] = 1.0;
+	macierz[2][3] = 2.0;
 	
-	for (k = 0; k < n; k++) {
+	for (k = 0; k < n-1; k++) {
 		
 		for (i = 0; i < n; i++) {
 			
-			temp = macierz[k][i]/macierz[k][k];
-			temp = temp * macierz[i][k];
-			//macierz[i][k] = macierz[i][k] - temp; - wrong; HOW TO OMMIT THE K-TH ROW?
-		
+			// temp holds k-th equation
+			
+			for (j = 0; j < n; j++) {
+				
+				temp[j] = macierz[k][j];
+				
+			}
+			
+			// temp divided by macierz[k][k]
+			
+			for (j = 0; j < n; j++) {
+				
+				temp[j] = temp[j] / macierz[k][k];
+				
+			}
+			
+			// temp multiplied by macierz[i][k]
+			
+			for (j = 0; j < n; j++) {
+				
+				temp[j] = temp[j] * macierz[i][k];
+				
+			}
+			
+			for (j = 0; j < n; j++) {
+				
+				if (i != k) {
+					
+					macierz[i][j] = macierz[i][j] - temp[j];
+				}
+				
+			}
 		}
+		
 	}
 	
-	/*for (i = 0; i < n; i++) {
+	
+	for (i = 0; i < n-1; i++) {
 		
-		for (j = 0; j < n; j++) {
-			
-			printf ("%f ", macierz[i][j]);
-			
-		}
-		printf ("\n");
-	} */
+		wektor[i] = macierz[i][n-1] / macierz[i][i];
+		printf ("%f\n", wektor[i]);
+	}
 	
 	
 	return 0;
