94b65e9eadf2d2f67e0725fddf120c1ac4de4eb9
hasiek <hasiek01@gmail.com>
2012-10-21 15:07:26 +0200

    Poprawiony Rozszerzony Algorytm Euklidesa.

--- Krypto/EEuklides.c
+++ Krypto/EEuklides.c
@@ -1,3 +1,6 @@
+// Rozszerzony Algorytm Euklidesa, Kryptografia - laboratorium 1
+// Katarzyna Huszcza, 21.10.2012
+
 #include <stdio.h>
 
 int main() {
@@ -6,8 +9,9 @@ int main() {
 	int x, x1, x2, y, y1, y2;
 	int aRead, bRead;
 	
-	aRead = 1547;
-	bRead = 560;
+	scanf ("%d %d", &aRead, &bRead);
+	
+	// Sprawdzenie, czy a > b, jeśli nie, zamiana a z b przy użyciu pustej zmiennej
 	
 	if (bRead > aRead) {
 	
@@ -16,6 +20,8 @@ int main() {
 	aRead = nwd;
 	
 	}
+	
+	// Przypisanie sczytanych wartości a i b do zmiennych roboczych, aby zachować wartości początkowe
 	
 	a = aRead;
 	b = bRead;
@@ -33,8 +39,12 @@ int main() {
 	
 	while (r != 0) {
 		
+		// Algorytm Euklidesa
+		
 		a = b;
 		b = r;
+		
+		// Przechowywanie wsp. kombinacji liniowej - x2 sprzed dwóch iteracji, x1 sprzed jednej, x obecny
 		 
 		x = x2 - q*x1;
 		x2 = x1;
@@ -42,7 +52,9 @@ int main() {
 		 
 		y = y2 - q*y1;
 		y2 = y1;
-		y1 = y;     
+		y1 = y; 
+		
+		// Algorytm Euklidesa    
 		 
 		nwd = r;
 		q = a / b;
@@ -50,7 +62,16 @@ int main() {
 	
 	}
 	
-	printf("NWD(%d, %d) = %d, kombinacja liniowa: %d * %d + %d * %d\n", aRead, bRead, nwd, x, aRead, y, bRead);
+	if (y < 0) {
 		
+		printf("NWD(%d, %d) = %d, kombinacja liniowa: %d * %d + (%d) * %d\n", aRead, bRead, nwd, x, aRead, y, bRead);
+		
+	}
+	else {
+		
+		printf("NWD(%d, %d) = %d, kombinacja liniowa: %d * %d + %d * %d\n", aRead, bRead, nwd, x, aRead, y, bRead);
+		
+	}
+	
 	return 0;
 }
